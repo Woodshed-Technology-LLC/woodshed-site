0d95fc89b087a1d02fe128340f0a7a4d5a3e558e
Alex Nitta <alex@woodshed.tech>
2023-04-04 06:44:48 -0700

    update Tailwind config; start building footer

---
+++ src/utils/getTailwindColorsFromTokens.js
@@ -0,0 +1,301 @@
+// This is copied from the Figma Tokens Studio plugin when viewing the Core set.
+const colors = {
+    grays: {
+        blue: {
+            'GrayDark 100': {
+                value: '#d1ecfa',
+                type: 'color',
+            },
+            'GrayDark 200': {
+                value: '#c6d9e1',
+                type: 'color',
+            },
+            'GrayDark 300': {
+                value: '#a3bec8',
+                type: 'color',
+            },
+            'GrayDark 400': {
+                value: '#8ba6b1',
+                type: 'color',
+            },
+            'GrayDark 500': {
+                value: '#668499',
+                type: 'color',
+            },
+            'GrayDark 600': {
+                value: '#517390',
+                type: 'color',
+            },
+            'GrayDark 700': {
+                value: '#33577a',
+                type: 'color',
+            },
+            'GrayDark 800': {
+                value: '#24385b',
+                type: 'color',
+            },
+            'GrayDark 900': {
+                value: '#0b214c',
+                type: 'color',
+            },
+        },
+        'blue shade': {
+            'GrayDarkShade 100': {
+                value: '#7a95bd',
+                type: 'color',
+            },
+            'GrayDarkShade 200': {
+                value: '#607da9',
+                type: 'color',
+            },
+            'GrayDarkShade 300': {
+                value: '#4a6996',
+                type: 'color',
+            },
+            'GrayDarkShade 400': {
+                value: '#35558d',
+                type: 'color',
+            },
+            'GrayDarkShade 500': {
+                value: '#284277',
+                type: 'color',
+            },
+            'GrayDarkShade 600': {
+                value: '#1f3561',
+                type: 'color',
+            },
+            'GrayDarkShade 700': {
+                value: '#152951',
+                type: 'color',
+            },
+            'GrayDarkShade 800': {
+                value: '#102142',
+                type: 'color',
+            },
+            'GrayDarkShade 900': {
+                value: '#071531',
+                type: 'color',
+            },
+        },
+        taupe: {
+            'GrayLight 100': {
+                value: '#fdfdfc',
+                type: 'color',
+            },
+            'GrayLight 200': {
+                value: '#f6f6f3',
+                type: 'color',
+            },
+            'GrayLight 300': {
+                value: '#f2f1ee',
+                type: 'color',
+            },
+            'GrayLight 400': {
+                value: '#ebebe5',
+                type: 'color',
+            },
+            'GrayLight 500': {
+                value: '#e1dfd6',
+                type: 'color',
+            },
+            'GrayLight 600': {
+                value: '#d8d6ca',
+                type: 'color',
+            },
+            'GrayLight 700': {
+                value: '#CBCABD',
+                type: 'color',
+            },
+            'GrayLight 800': {
+                value: '#bab8ab',
+                type: 'color',
+            },
+            'GrayLight 900': {
+                value: '#adab9f',
+                type: 'color',
+            },
+        },
+        'taupe shade': {
+            'GrayLightShade 100': {
+                value: '#6a6a62',
+                type: 'color',
+            },
+            'GrayLightShade 200': {
+                value: '#595954',
+                type: 'color',
+            },
+            'GrayLightShade 300': {
+                value: '#505049',
+                type: 'color',
+            },
+            'GrayLightShade 400': {
+                value: '#46463f',
+                type: 'color',
+            },
+            'GrayLightShade 500': {
+                value: '#3b3b35',
+                type: 'color',
+            },
+            'GrayLightShade 600': {
+                value: '#363630',
+                type: 'color',
+            },
+            'GrayLightShade 700': {
+                value: '#2e2d29',
+                type: 'color',
+            },
+            'GrayLightShade 800': {
+                value: '#252522',
+                type: 'color',
+            },
+            'GrayLightShade 900': {
+                value: '#1b1b18',
+                type: 'color',
+            },
+        },
+    },
+    primary: {
+        azure: {
+            'PrimaryDark 100': {
+                value: '#e4f9fc',
+                type: 'color',
+            },
+            'PrimaryDark 200': {
+                value: '#bff1f8',
+                type: 'color',
+            },
+            'PrimaryDark 300': {
+                value: '#a4e6ef',
+                type: 'color',
+            },
+            'PrimaryDark 400': {
+                value: '#6dd2df',
+                type: 'color',
+            },
+            'PrimaryDark 500': {
+                value: '#2ac4d5',
+                type: 'color',
+            },
+            'PrimaryDark 600': {
+                value: '#1bb4c5',
+                type: 'color',
+            },
+            'PrimaryDark 700': {
+                value: '#22a5b9',
+                type: 'color',
+            },
+            'PrimaryDark 800': {
+                value: '#1f98a8',
+                type: 'color',
+            },
+            'PrimaryDark 900': {
+                value: '#1d839a',
+                type: 'color',
+            },
+        },
+        green: {
+            'PrimaryLight 100': {
+                value: '#98cdb3',
+                type: 'color',
+            },
+            'PrimaryLight 200': {
+                value: '#75BD9B',
+                type: 'color',
+            },
+            'PrimaryLight 300': {
+                value: '#64b48f',
+                type: 'color',
+            },
+            'PrimaryLight 400': {
+                value: '#5dac86',
+                type: 'color',
+            },
+            'PrimaryLight 500': {
+                value: '#439d72',
+                type: 'color',
+            },
+            'PrimaryLight 600': {
+                value: '#3c8b65',
+                type: 'color',
+            },
+            'PrimaryLight 700': {
+                value: '#367d5e',
+                type: 'color',
+            },
+            'PrimaryLight 800': {
+                value: '#2b5f47',
+                type: 'color',
+            },
+            'PrimaryLight 900': {
+                value: '#295642',
+                type: 'color',
+            },
+        },
+        'green shade': {
+            'PrimaryLightShade 100': {
+                value: '#d1e0d2',
+                type: 'color',
+            },
+            'PrimaryLightShade 200': {
+                value: '#c2d6c3',
+                type: 'color',
+            },
+            'PrimaryLightShade 300': {
+                value: '#b4cbb5',
+                type: 'color',
+            },
+            'PrimaryLightShade 400': {
+                value: '#a8c2a9',
+                type: 'color',
+            },
+            'PrimaryLightShade 500': {
+                value: '#9eb79f',
+                type: 'color',
+            },
+            'PrimaryLightShade 600': {
+                value: '#8aa88b',
+                type: 'color',
+            },
+            'PrimaryLightShade 700': {
+                value: '#86a287',
+                type: 'color',
+            },
+            'PrimaryLightShade 800': {
+                value: '#7d9b7e',
+                type: 'color',
+            },
+            'PrimaryLightShade 900': {
+                value: '#779278',
+                type: 'color',
+            },
+        },
+    },
+};
+
+// Map the colors from the Figma Tokens Studio plugin into the format that Tailwind expects. The
+// strings are not perfectly formatted in terms of casing, but that's easy to fix manually.
+// We don't use this in the application code; it's just used for copying the tokens over into
+// tailwind.config.js.
+function main() {
+    const result = {};
+
+    Object.values(colors).forEach(layer0 => {
+        Object.values(layer0).forEach(layer1 => {
+            Object.entries(layer1).forEach(([key, value]) => {
+                const keys = key.split(' ');
+                const family = keys[0].trim();
+                const grade = parseInt(keys[1].trim());
+
+                if (result[family] === undefined) {
+                    result[family] = {};
+                }
+
+                result[family][grade] = value.value;
+            });
+        });
+    });
+
+    console.log(JSON.stringify(result, null, 4));
+}
+
+main();
