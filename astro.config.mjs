e28376182f4e0fe61c2ad84e43734a5dca7dd78f
Alex Nitta <alex@woodshed.tech>
2024-01-16 15:01:29 -0800

    ci: remove astro-compress plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-    compressHTML: false,
     integrations: [
         mdx(),
         tailwind({
@@ -15,8 +13,6 @@ export default defineConfig({
             },
         }),
         sitemap(),
-        // Turn off CSS compression because it causes bugs in the NavHeader position
-        compress({ CSS: false }),
     ],
     sitemap: true,
     site: 'https://woodshed.tech',
