b09ab8b284852138135b0809fe19b095332aea69
Alex Nitta <alex@woodshed.tech>
2024-01-16 14:31:41 -0800

    fix: upgrade to Astro 3; update more content

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        assets: true,
-    },
     integrations: [
         mdx(),
         tailwind({
